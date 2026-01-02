cdc7f2e173874cee5387ba70b297c145364c0b65
nacho <109104298+Zadiu@users.noreply.github.com>
2026-01-02 16:13:23 -0300

    asd

--- app/main.py
+++ app/main.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
-from app.db.database import engine
 from app.db.database import Base, engine
 from app.models import user
 from app.routes import auth
 
 app = FastAPI(title="Medical API")
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
-
-@app.on_event("startup")
-def startup():
-    engine.connect()
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 
-Base.metadata.create_all(bind=engine)
+@app.get("/")
+def root():
+    return {"status": "ok"}
